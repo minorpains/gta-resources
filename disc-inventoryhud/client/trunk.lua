b01d7806e90359eba12f067aa84134edffa26b37
minorpains <aunder6235@gmail.com>
2019-12-05 13:11:07 -0800

    Add files via upload

--- disc-inventoryhud/client/trunk.lua
+++ disc-inventoryhud/client/trunk.lua
@@ -9,40 +9,39 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
         if IsControlJustReleased(0, Config.TrunkOpenControl) then
-            local playerPed = PlayerPedId()
             local vehicle = ESX.Game.GetVehicleInDirection()
-            if DoesEntityExist(vehicle) and GetVehiclePedIsIn(playerPed) == 0 then
-                local locked = GetVehicleDoorLockStatus(vehicle) == 2
+            if DoesEntityExist(vehicle) then
+                local locked = GetVehicleDoorsLockedForPlayer(vehicle) == 1
+                print(locked)
                 local hasBoot = DoesVehicleHaveDoor(vehicle, 5)
+                local boneIndex = nil
+                local vehicleCoords = nil
+                local distance = nil
                 if not locked then
-                    local boneIndex = GetEntityBoneIndexByName(vehicle, 'platelight')
-                    local vehicleCoords = GetWorldPositionOfEntityBone(vehicle, boneIndex)
-                    local playerCoords = GetEntityCoords(playerPed)
-                    local distance = GetDistanceBetweenCoords(vehicleCoords.x, vehicleCoords.y, vehicleCoords.z, playerCoords.x, playerCoords.y, playerCoords.z, true)
+                    local playerCoords = GetEntityCoords(GetPlayerPed(-1))
+                    if ESX.Game.GetVehicleProperties(vehicle).model == -877478386 then
+                        boneIndex = GetEntityBoneIndexByName(vehicle, 'brakelight_l')
+                        vehicleCoords = GetWorldPositionOfEntityBone(vehicle, boneIndex)
+                        distance = GetDistanceBetweenCoords(vehicleCoords.x, vehicleCoords.y, vehicleCoords.z, playerCoords.x, playerCoords.y, playerCoords.z, true)
+                    else
+                        boneIndex = GetEntityBoneIndexByName(vehicle, 'platelight')
+                        vehicleCoords = GetWorldPositionOfEntityBone(vehicle, boneIndex)
+                        distance = GetDistanceBetweenCoords(vehicleCoords.x, vehicleCoords.y, vehicleCoords.z, playerCoords.x, playerCoords.y, playerCoords.z, true)
+                    end
+
                     if distance < 3 then
                         trunkSecondaryInventory.owner = GetVehicleNumberPlateText(vehicle)
                         openVehicle = vehicle
-                        inventoryType = 'trunk-'
-                        local name = GetDisplayNameFromVehicleModel(GetEntityModel(vehicle))
-                        ESX.TriggerServerCallback('disc-inventoryhud:doesInvTypeExists', function(exists)
-                            print(inventoryType .. name)
-                            print(exists)
-                            if (exists) then
-                                inventoryType = inventoryType .. name
-                            else
-                                local class = GetVehicleClass(vehicle)
-                                inventoryType = inventoryType .. class
-                            end
-                            trunkSecondaryInventory.type = inventoryType
-                            SetVehicleDoorOpen(openVehicle, 5, false)
-                            openInventory(trunkSecondaryInventory)
-                            local playerPed = GetPlayerPed(-1)
-                            if not IsEntityPlayingAnim(playerPed, 'mini@repair', 'fixing_a_player', 3) then
-                                ESX.Streaming.RequestAnimDict('mini@repair', function()
-                                    TaskPlayAnim(playerPed, 'mini@repair', 'fixing_a_player', 8.0, -8, -1, 49, 0, 0, 0, 0)
-                                end)
-                            end
-                        end, inventoryType .. name)
+                        local class = GetVehicleClass(vehicle)
+                        trunkSecondaryInventory.type = 'trunk-' .. class
+                        SetVehicleDoorOpen(openVehicle, 5, false)
+                        openInventory(trunkSecondaryInventory)
+                        local playerPed = GetPlayerPed(-1)
+                        if not IsEntityPlayingAnim(playerPed, 'mini@repair', 'fixing_a_player', 3) then
+                            ESX.Streaming.RequestAnimDict('mini@repair', function()
+                                TaskPlayAnim(playerPed, 'mini@repair', 'fixing_a_player', 8.0, -8, -1, 49, 0, 0, 0, 0)
+                            end)
+                        end
                     end
                 end
             end
